6690227de76db8154ef60d2fde6c0ad379228bbf
--replace-all <kaique.mendonca@cenpros.com.br>
2023-03-14 17:00:26 -0300

    Exibindo dados de produto unico

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,12 +1,28 @@
 import {useParams} from "react-router-dom";
+import {useFetch} from "../hooks/useFetch";
 
 const Product = ()=> {
 
+    // getting id from url
     const { id } = useParams()
+
+    // getting data of item
+    const url = "http://localhost:3000/products/"+id
+    const {data: product, loading, error} = useFetch(url)
+
+    console.log(product)
 
     return (
         <>
             <p>{id}</p>
+            {error && <p>Ocorreu um erro...</p>}
+            {loading && <p>Carregando...</p>}
+            {product && (
+                <div>
+                    <h1>{product.name}</h1>
+                    <p>R$:{product.price}</p>
+                </div>
+            )}
         </>
     )
 }
